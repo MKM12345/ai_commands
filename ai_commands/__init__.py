3c65518f85aaeb92fd13e0a2ba0cfddf1c0e4153
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-09-24 15:40:04 +0100

    Update __init__.py

--- ai_commands/__init__.py
+++ ai_commands/__init__.py
@@ -1,1 +1,12 @@
+from .ai_commands import ai_similarise, analyze_sentiment, translate_text, ai_summarize, detect_language, generate_image_caption, ner_extraction
+
+__all__ = [
+    "ai_similarise",
+    "analyze_sentiment",
+    "translate_text",
+    "ai_summarize",
+    "detect_language",
+    "generate_image_caption",
+    "ner_extraction"
+]
 
