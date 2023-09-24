8bb24eb4f2cdab18682c95f8db354942fe79b958
MKM12345 <120132998+MKM12345@users.noreply.github.com>
2023-09-24 18:41:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.1.0'
 DESCRIPTION = 'A Python package for simple but complex computer functions.'
 LONG_DESCRIPTION = """
 <div align="center">
